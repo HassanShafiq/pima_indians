a1e129725530b7f98138df5110a681a96a9bb99f
Hassan Shafiq <ciao.hassanshafiq@gmail.com>
2019-11-29 21:26:58 +0500

    Update pima_yaml.py

--- pima_yaml.py
+++ pima_yaml.py
@@ -8,6 +8,7 @@ from keras.layers import Dense
 from keras.models import model_from_yaml
 import numpy
 import os
+
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
@@ -17,36 +18,43 @@ dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:8]
 Y = dataset[:,8]
+
 # create model
 model = Sequential()
 model.add(Dense(12, input_dim=8, activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
+
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#Model Summary
+model.summary()
+
 # Fit the model
 model.fit(X, Y, epochs=150, batch_size=10, verbose=0)
+
 # evaluate the model
 scores = model.evaluate(X, Y, verbose=0)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 # serialize model to YAML
 model_yaml = model.to_yaml()
-with open("model.yaml", "w") as yaml_file:
+with open("pima_model.yaml", "w") as yaml_file:
     yaml_file.write(model_yaml)
 # serialize weights to HDF5
-model.save_weights("model.h5")
+model.save_weights("pima_weights.h5")
 print("Saved model to disk")
 
 # later...
 
 # load YAML and create model
-yaml_file = open('model.yaml', 'r')
+yaml_file = open('pima_model.yaml', 'r')
 loaded_model_yaml = yaml_file.read()
 yaml_file.close()
 loaded_model = model_from_yaml(loaded_model_yaml)
 # load weights into new model
-loaded_model.load_weights("model.h5")
+loaded_model.load_weights("pima_weights.h5")
 print("Loaded model from disk")
 
 # evaluate loaded model on test data
